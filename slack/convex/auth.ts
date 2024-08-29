54b0e3930afa5354e5b22855c3b72805677ec1f9
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-08-29 18:25:44 -0300

    feat: add userButton component

--- slack/convex/auth.ts
+++ slack/convex/auth.ts
@@ -2,7 +2,18 @@ import Github from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
 import { convexAuth } from "@convex-dev/auth/server";
 import { Password } from "@convex-dev/auth/providers/Password";
+import {DataModel} from "./_generated/dataModel";
+
+
+const CustomPassword = Password<DataModel>({
+  profile(params){
+    return {
+      email: params.email as string,
+      name: params.name as string,
+    }
+  }
+})
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [Password, Github, Google],
+  providers: [CustomPassword, Github, Google],
 });
--- slack/src/app/page.tsx
+++ slack/src/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { useAuthActions } from "@convex-dev/auth/react";
+import {UserButton} from "@/features/auth/components/user-button";
 
 export default function Home() {
 
-    const { signOut } = useAuthActions();
-
     return (
         <div>
-            Logged in!
-            <button onClick={signOut}>sair</button>
+            <UserButton/>
         </div>
     );
 }
