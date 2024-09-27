8604ca945bcc048b1a3806aca152ddba7bfba422
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-09-27 11:56:13 -0300

    feat: add component channel header

--- slack/convex/channels.ts
+++ slack/convex/channels.ts
@@ -3,6 +3,83 @@ import {mutation, query} from "./_generated/server";
 import {getAuthUserId} from "@convex-dev/auth/server";
 
 
+export const remove = mutation({
+    args: {
+        id: v.id("channels"),
+    },
+    handler: async (ctx, args) => {
+        const userId = await getAuthUserId(ctx);
+
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const channel = await ctx.db.get(args.id);
+
+        if(!channel){
+            throw new Error("Channel not found");
+        }
+
+        const member = await ctx.db
+            .query("members")
+            .withIndex("by_workspace_id_user_id",
+                (q) => q.eq("workspaceId", channel.workspaceId)
+                    .eq("userId", userId)
+            )
+            .unique()
+        ;
+
+        if (!member || member.role !== "admin") {
+            throw new Error("Unauthorized");
+        }
+
+
+
+        await ctx.db.delete(args.id);
+
+        return args.id;
+    }
+})
+
+export const update = mutation({
+    args: {
+        id: v.id("channels"),
+        name: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const userId = await getAuthUserId(ctx);
+
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const channel = await ctx.db.get(args.id);
+
+        if(!channel){
+            throw new Error("Channel not found");
+        }
+
+        const member = await ctx.db
+            .query("members")
+            .withIndex("by_workspace_id_user_id",
+                (q) => q.eq("workspaceId", channel.workspaceId)
+                    .eq("userId", userId)
+            )
+            .unique()
+        ;
+
+        if (!member || member.role !== "admin") {
+            throw new Error("Unauthorized");
+        }
+
+        await ctx.db.patch(args.id, {
+            name: args.name
+        });
+
+        return args.id;
+    }
+});
+
 export const create = mutation({
     args: {
         name: v.string(),
@@ -11,7 +88,7 @@ export const create = mutation({
     handler: async (ctx, args) => {
         const userId = await getAuthUserId(ctx);
 
-        if(!userId){
+        if (!userId) {
             throw new Error("Unauthorized");
         }
 
@@ -24,7 +101,7 @@ export const create = mutation({
             .unique()
         ;
 
-        if(!member || member.role !== "admin"){
+        if (!member || member.role !== "admin") {
             throw new Error("Unauthorized");
         }
 
@@ -38,6 +115,39 @@ export const create = mutation({
             workspaceId: args.workspaceId
         });
     }
+});
+
+export const getById = query({
+    args: {
+        id: v.id("channels"),
+    },
+    handler: async (ctx, args) => {
+        const userId = await getAuthUserId(ctx);
+
+        if (!userId) {
+            return null;
+        }
+
+        const channel = await ctx.db.get(args.id);
+
+        if (!channel) {
+            return null;
+        }
+
+        const member = await ctx.db
+            .query("members")
+            .withIndex("by_workspace_id_user_id", (q) =>
+                q.eq("workspaceId", channel.workspaceId).eq("userId", userId))
+            .unique()
+        ;
+
+        if (!member) {
+            return null;
+        }
+
+        return channel;
+
+    }
 })
 
 export const get = query({
@@ -47,7 +157,7 @@ export const get = query({
     handler: async (ctx, args) => {
         const userId = await getAuthUserId(ctx);
 
-        if(!userId){
+        if (!userId) {
             return [];
         }
 
@@ -60,16 +170,16 @@ export const get = query({
             .unique()
         ;
 
-        if(!member){
+        if (!member) {
             return [];
         }
 
         return await ctx.db
             .query("channels")
             .withIndex("by_workspace_id", (q) =>
-            q.eq("workspaceId", args.workspaceId))
+                q.eq("workspaceId", args.workspaceId))
             .collect()
-        ;
+            ;
 
     }
 })
--- slack/src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ slack/src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,11 +1,39 @@
+"use client";
 
+import {useChannelId} from "@/hooks/use-channel-id";
+import {useGetChannel} from "@/features/channels/api/use-get-channel";
+import {Loader, TriangleAlert} from "lucide-react";
+import {Header} from "./header";
 
 const ChannelPage = () => {
+    const channelId = useChannelId();
+
+    const {data: channel, isLoading: channelLoading} = useGetChannel({id: channelId});
+
+    if(channelLoading) {
+        return(
+            <div className="h-full flex-1 flex items-center justify-center">
+                <Loader className="size-5 animate-spin text-muted-foreground" />
+            </div>
+        );
+    }
+
+    if(!channel){
+        return(
+            <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+                <TriangleAlert className="size-5 animate-bounce text-muted-foreground" />
+                <span className="text-sm text-muted-foreground">Channel not found</span>
+            </div>
+        );
+    }
+
     return (
-        <div>
-            1
+        <div className="flex flex-col h-full">
+            <Header title={channel.name} />
         </div>
     )
 }
 
 export default ChannelPage;
+
+//06:58:18
