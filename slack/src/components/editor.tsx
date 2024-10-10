6d137e33f7926b30dea1129b11efc113ab0e5855
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-10-10 17:24:11 -0300

    feat: add feature image picker

--- slack/src/components/editor.tsx
+++ slack/src/components/editor.tsx
@@ -1,8 +1,10 @@
-import {MutableRefObject, useEffect, useLayoutEffect, useMemo, useRef, useState} from "react";
+import {MutableRefObject, useEffect, useLayoutEffect, useRef, useState} from "react";
+import Image from 'next/image';
+
 import {PiTextAa} from "react-icons/pi";
 import Quill, {QuillOptions} from 'quill';
 import {Button} from "@/components/ui/button";
-import {ImageIcon, Smile} from "lucide-react";
+import {ImageIcon, Smile, XIcon} from "lucide-react";
 import {MdSend} from "react-icons/md";
 import {Hint} from './hint';
 import {Delta, Op} from "quill/core";
@@ -40,6 +42,7 @@ const Editor = ({
                     innerRef
                 }: EditorProps) => {
     const [text, setText] = useState("");
+    const [image, setImage] = useState<File | null>(null);
     const [isToolbarVisible, setToolbarVisible] = useState(true);
 
     const containerRef = useRef<HTMLDivElement>(null);
@@ -48,6 +51,7 @@ const Editor = ({
     const quillRef = useRef<Quill | null>(null);
     const defaultValueRef = useRef(defaultValue);
     const disabledRef = useRef(disabled);
+    const imageElementRef = useRef<HTMLInputElement>(null);
 
     useLayoutEffect(() => {
         submitRef.current = onSubmit;
@@ -146,10 +150,40 @@ const Editor = ({
 
     return (
         <div className="flex flex-col">
+            <input
+                type="file"
+                accept="image/*"
+                ref={imageElementRef}
+                onChange={(event) => setImage(event.target.files![0])}
+                className="hidden"
+            />
             <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300
                 focus-within:shadow-sm transition bg-white"
             >
                 <div ref={containerRef} className="h-full ql-custom"/>
+                {!!image && (
+                    <div className="p-2">
+                        <div className="relative size-[62px] flex items-center justify-center group/image">
+                            <Hint label="remove image">
+                                <button
+                                    onClick={() => {
+                                        setImage(null);
+                                        imageElementRef.current!.value = "";
+                                    }}
+                                    className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center"
+                                >
+                                    <XIcon className="size-3.5"/>
+                                </button>
+                            </Hint>
+                            <Image
+                                src={URL.createObjectURL(image)}
+                                alt="Upload image"
+                                fill
+                                className="rounded-xl overflow-hidden border object-cover"
+                            />
+                        </div>
+                    </div>
+                )}
                 <div className="flex px-2 pb-2 z-[5]">
                     <Hint label={isToolbarVisible ? "Hide formatting" : "Show formatting"}>
                         <Button
@@ -176,8 +210,7 @@ const Editor = ({
                                 disabled={disabled}
                                 size="iconSm"
                                 variant="ghost"
-                                onClick={() => {
-                                }}
+                                onClick={() => imageElementRef.current?.click()}
                             >
                                 <ImageIcon className="size-4"/>
                             </Button>
@@ -229,7 +262,6 @@ const Editor = ({
                 <div className={cn(
                     "p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition",
                     !isEmpty && "opacity-100",
-
                 )}>
                     <p>
                         <strong>Shift + Return</strong> to add a new line
