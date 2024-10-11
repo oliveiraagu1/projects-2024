a069c03ae8d209e4f1f9a6d709aa84a7aa45a588
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-10-11 19:48:34 -0300

    feat: add schema finalization

--- slack/convex/schema.ts
+++ slack/convex/schema.ts
@@ -1,6 +1,6 @@
-import { v } from "convex/values";
-import { defineSchema, defineTable } from "convex/server";
-import { authTables } from "@convex-dev/auth/server";
+import {v} from "convex/values";
+import {defineSchema, defineTable} from "convex/server";
+import {authTables} from "@convex-dev/auth/server";
 
 const schema = defineSchema({
     ...authTables,
@@ -23,6 +23,12 @@ const schema = defineSchema({
 
     })
         .index("by_workspace_id", ["workspaceId"]),
+    conversations: defineTable({
+        workspaceId: v.id("workspaces"),
+        memberOneId: v.id("members"),
+        memberTwoId: v.id("members"),
+    })
+        .index("by_workspace_id", ["workspaceId"]),
     messages: defineTable({
         body: v.string(),
         image: v.optional(v.id("_storage")),
@@ -30,8 +36,27 @@ const schema = defineSchema({
         workspaceId: v.id("workspaces"),
         channelId: v.optional(v.id("channels")),
         parentMessageId: v.optional(v.id("messages")),
+        conversationId: v.optional(v.id("conversations")),
         updatedAt: v.number(),
-    }),
+    })
+        .index("by_workspace_id", ["workspaceId"])
+        .index("by_member_id", ["memberId"])
+        .index("by_channel_id", ["channelId"])
+        .index("by_conversation_id", ["conversationId"])
+        .index("by_channel_id_parent_message_id_conversation_id", [
+            "channelId",
+            "parentMessageId",
+            "conversationId"
+        ]),
+    reactions: defineTable({
+        workspaceId: v.id("workspaces"),
+        messageId: v.id("messages"),
+        memberId: v.id("members"),
+        values: v.string()
+    })
+        .index("by_workspace_id", ["workspaceId"])
+        .index("by_message_id", ["messageId"])
+        .index("by_member_id", ["memberId"])
 });
 
 export default schema;
