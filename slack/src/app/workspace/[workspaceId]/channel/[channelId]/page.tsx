cd2d28172aa210c2b2cdd82d4d571aefe65898d2
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-10-11 21:02:02 -0300

    feat: add messages GET API

--- slack/src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ slack/src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,24 +5,26 @@ import {useGetChannel} from "@/features/channels/api/use-get-channel";
 import {Loader, TriangleAlert} from "lucide-react";
 import {Header} from "./header";
 import {ChatInput} from "./chat-input";
+import {useGetMessages} from "@/features/messages/use-get-messages";
 
 const ChannelPage = () => {
     const channelId = useChannelId();
 
+    const { results } = useGetMessages({channelId});
     const {data: channel, isLoading: channelLoading} = useGetChannel({id: channelId});
 
-    if(channelLoading) {
-        return(
+    if (channelLoading) {
+        return (
             <div className="h-full flex-1 flex items-center justify-center">
-                <Loader className="size-5 animate-spin text-muted-foreground" />
+                <Loader className="size-5 animate-spin text-muted-foreground"/>
             </div>
         );
     }
 
-    if(!channel){
-        return(
+    if (!channel) {
+        return (
             <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
-                <TriangleAlert className="size-5 animate-bounce text-muted-foreground" />
+                <TriangleAlert className="size-5 animate-bounce text-muted-foreground"/>
                 <span className="text-sm text-muted-foreground">Channel not found</span>
             </div>
         );
@@ -30,9 +32,11 @@ const ChannelPage = () => {
 
     return (
         <div className="flex flex-col h-full">
-            <Header title={channel.name} />
-            <div className="flex-1"/>
-            <ChatInput placeholder={`Message: # ${channel.name}`} />
+            <Header title={channel.name}/>
+            <div className="flex-1">
+                {JSON.stringify(results, null, 2)}
+            </div>
+            <ChatInput placeholder={`Message: # ${channel.name}`}/>
         </div>
     )
 }
